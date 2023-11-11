3f9ff4e44cbd82e2bd2d7789a511977c13a41cb2
Sebastián Suárez <sebastian.suarezr@udea.edu.co>
2023-11-10 21:19:42 -0500

    add Poppins font to global styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,16 @@
 import { Sidebar } from '@/components/layouts/Sidebar';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import { Poppins } from 'next/font/google';
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800'],
+});
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <div className='main-layout'>
+    <div className={`${poppins.className} main-layout`}>
       <Sidebar />
       <Component {...pageProps} />
     </div>
