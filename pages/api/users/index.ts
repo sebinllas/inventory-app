d7d313f6843648a023bdddcaa620fbb405a50799
Sebastián Suárez <sebastian.suarezr@udea.edu.co>
2023-11-16 13:53:16 -0500

    add endpoint to get user by email

--- pages/api/users/index.ts
+++ pages/api/users/index.ts
@@ -12,6 +12,21 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   await checkAuth(req, res, ['ADMIN']);
 
   if (req.method === AllowedMethods.GET) {
+    if (req.query['email']) {
+      const email = req.query['email'] as string;
+      const user = await prisma.$queryRaw<UserResponse[]>`
+        SELECT u.* , r."name" AS "role"
+        FROM "User" u
+        FULL OUTER JOIN "Role" r
+        ON r.id  = u."roleId"
+        WHERE u.id IS NOT NULL AND u.email = ${email}
+        ORDER BY r.id
+    `;
+      if (user.length === 0) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+      return res.status(200).json(user[0]);
+    }
     const users = await prisma.$queryRaw<UserResponse[]>`
     		SELECT u.* , r."name" AS "role"
     		FROM "User" u
